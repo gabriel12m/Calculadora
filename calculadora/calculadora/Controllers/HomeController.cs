7d56643b1f1f84da7b918b5e0a6dd646994ca8c0
Gabriel Marcal <aluno21090@ipt.pt>
2020-03-18 16:30:12 +0000

    Início do código do controller

--- calculadora/calculadora/Controllers/HomeController.cs
+++ calculadora/calculadora/Controllers/HomeController.cs
@@ -24,8 +24,28 @@ namespace calculadora.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string visor)
+        public IActionResult Index(string visor, string bt)
         {
+            switch (bt)
+            {
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    if (visor == "0") visor = bt;
+                    else visor += bt;
+                    break;
+            }
+
+            //enviar dados do Visor para a view
+
+
             return View();
         }
 
